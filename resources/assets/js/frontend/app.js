be377fe71eacfcee9b3d399c44f2676e9680a11a
Pathagama Kuruppuge Tharindu <pktharindu@outlook.com>
2017-08-06 10:03:03 +0530

    Hide header on scroll down, show on scroll up

--- resources/assets/js/frontend/app.js
+++ resources/assets/js/frontend/app.js
@@ -20,3 +20,45 @@ Vue.component('example', require('../components/frontend/Example.vue'));
 const app = new Vue({
     el: '#app'
 });
+
+/*
+ * Hide header on scroll down, show on scroll up
+ */
+ 
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
