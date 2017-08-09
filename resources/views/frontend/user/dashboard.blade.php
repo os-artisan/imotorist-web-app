d58c29802193dbf65f84104593165fb0d2ff3f6a
Pathagama Kuruppuge Tharindu <pktharindu@outlook.com>
2017-08-09 17:42:17 +0530

    ignore .key files

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -5,138 +5,60 @@
     <div class="container">
 
         <div class="row">
-        
-            <div class="col-xs-12">
-        
+
+            <div class="col-md-3">
+                <div class="profile-sidebar">
+                    <!-- SIDEBAR USERPIC -->
+                    <div class="profile-userpic">
+                        <img src="http://keenthemes.com/preview/metronic/theme/assets/admin/pages/media/profile/profile_user.jpg" class="img-responsive" alt="">
+                    </div>
+                    <!-- END SIDEBAR USERPIC -->
+                    <!-- SIDEBAR USER TITLE -->
+                    <div class="profile-usertitle">
+                        <div class="profile-usertitle-name">
+                            Marcus Doe
+                        </div>
+                        <div class="profile-usertitle-job">
+                            Developer
+                        </div>
+                    </div>
+                    <!-- END SIDEBAR USER TITLE -->
+                    <!-- SIDEBAR BUTTONS -->
+                    <div class="profile-userbuttons">
+                        <button type="button" class="btn btn-success btn-sm">Follow</button>
+                        <button type="button" class="btn btn-danger btn-sm">Message</button>
+                    </div>
+                    <!-- END SIDEBAR BUTTONS -->
+                    <!-- SIDEBAR MENU -->
+                    <div class="profile-usermenu">
+                        <ul class="nav">
+                            <li class="active">
+                                <a href="page_user_profile_1.html">
+                                <i class="icon-home"></i> Overview </a>
+                            </li>
+                            <li>
+                                <a href="page_user_profile_1_account.html">
+                                <i class="icon-settings"></i> Account Settings </a>
+                            </li>
+                            <li>
+                                <a href="page_user_profile_1_help.html">
+                                <i class="icon-info"></i> Help </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END MENU -->
+                </div>
+            </div>
+
+            <div class="col-md-9">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ trans('navs.frontend.dashboard') }}</div>
-        
                     <div class="panel-body">
-        
-                        <div class="row">
-        
-                            <div class="col-md-4 col-md-push-8">
-        
-                                <ul class="media-list">
-                                    <li class="media">
-                                        <div class="media-left">
-                                            <img class="media-object profile-picture" src="{{ $logged_in_user->picture }}" alt="Profile picture">
-                                        </div><!--media-left-->
-        
-                                        <div class="media-body">
-                                            <h4 class="media-heading">
-                                                {{ $logged_in_user->name }}<br/>
-                                                <small>
-                                                    {{ $logged_in_user->email }}<br/>
-                                                    Joined {{ $logged_in_user->created_at->format('F jS, Y') }}
-                                                </small>
-                                            </h4>
-        
-                                            {{ link_to_route('frontend.user.account', trans('navs.frontend.user.account'), [], ['class' => 'btn btn-info btn-xs']) }}
-        
-                                            @permission('view-backend')
-                                                {{ link_to_route('admin.dashboard', trans('navs.frontend.user.administration'), [], ['class' => 'btn btn-danger btn-xs']) }}
-                                            @endauth
-                                        </div><!--media-body-->
-                                    </li><!--media-->
-                                </ul><!--media-list-->
-        
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h4>Sidebar Item</h4>
-                                    </div><!--panel-heading-->
-        
-                                    <div class="panel-body">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                    </div><!--panel-body-->
-                                </div><!--panel-->
-        
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h4>Sidebar Item</h4>
-                                    </div><!--panel-heading-->
-        
-                                    <div class="panel-body">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                    </div><!--panel-body-->
-                                </div><!--panel-->
-                            </div><!--col-md-4-->
-        
-                            <div class="col-md-8 col-md-pull-4">
-                                <div class="row">
-                                    <div class="col-xs-12">
-                                        <div class="panel panel-default">
-                                            <div class="panel-heading">
-                                                <h4>Item</h4>
-                                            </div><!--panel-heading-->
-        
-                                            <div class="panel-body">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.</p>
-                                            </div><!--panel-body-->
-                                        </div><!--panel-->
-                                    </div><!--col-xs-12-->
-                                </div><!--row-->
-        
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="panel panel-default">
-                                            <div class="panel-heading">
-                                                <h4>Item</h4>
-                                            </div><!--panel-heading-->
-        
-                                            <div class="panel-body">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.</p>
-                                            </div><!--panel-body-->
-                                        </div><!--panel-->
-                                    </div><!--col-md-6-->
-        
-                                    <div class="col-md-6">
-                                        <div class="panel panel-default">
-                                            <div class="panel-heading">
-                                                <h4>Item</h4>
-                                            </div><!--panel-heading-->
-        
-                                            <div class="panel-body">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.</p>
-                                            </div><!--panel-body-->
-                                        </div><!--panel-->
-                                    </div><!--col-md-6-->
-        
-                                    <div class="col-md-6">
-                                        <div class="panel panel-default">
-                                            <div class="panel-heading">
-                                                <h4>Item</h4>
-                                            </div><!--panel-heading-->
-        
-                                            <div class="panel-body">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.</p>
-                                            </div><!--panel-body-->
-                                        </div><!--panel-->
-                                    </div><!--col-md-6-->
-        
-                                    <div class="col-md-6">
-                                        <div class="panel panel-default">
-                                            <div class="panel-heading">
-                                                <h4>Item</h4>
-                                            </div><!--panel-heading-->
-        
-                                            <div class="panel-body">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.</p>
-                                            </div><!--panel-body-->
-                                        </div><!--panel-->
-                                    </div><!--col-md-6-->
-        
-                                </div><!--row-->
-        
-                            </div><!--col-md-8-->
-        
-                        </div><!--row-->
-        
-                    </div><!--panel body-->
-        
-                </div><!-- panel -->
-        
-            </div><!-- col-md-10 -->
+                        <div class="profile-content">
+                           Some user related content goes here...
+                        </div>
+                    </div>
+                </div>
+            </div>
         
         </div><!-- row -->
 
