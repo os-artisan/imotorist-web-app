b33da42fae429ea360f226c7ab08e0682b74b056
Pathagama Kuruppuge Tharindu <pktharindu@outlook.com>
2017-10-16 11:45:50 +0000

    Apply fixes from StyleCI

--- routes/Api/Ticket.php
+++ routes/Api/Ticket.php
@@ -5,7 +5,6 @@
  * All route names are prefixed with 'api'.
  */
 Route::group(['namespace' => 'Ticket', 'middleware' => 'auth:api'], function () {
-
     Route::post('store-ticket', 'TicketController@store');
 
     Route::post('show-ticket', 'TicketController@show');
