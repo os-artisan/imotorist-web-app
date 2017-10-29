cd4671d513839f2c95faeedeb9a72e465007e184
Pathagama Kuruppuge Tharindu <pktharindu@outlook.com>
2017-10-29 19:17:38 +0530

    added email driver

--- app/Notifications/Api/Fine/UserReceivesTicket.php
+++ app/Notifications/Api/Fine/UserReceivesTicket.php
@@ -29,7 +29,7 @@ class UserReceivesTicket extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['mail', 'database'];
     }
 
     /**
